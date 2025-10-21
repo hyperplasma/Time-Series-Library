db041f200786753908d3f51d01dc976f6abe821f
Akira37 <863781751@qq.com>
2025-10-22 00:10:10 +0800

    Add an all-abl-exp script

---
+++ scripts/long_term_forecast/Weather_script/abl_NewTimesNet/exp.sh
@@ -0,0 +1,11 @@
+echo "=============================== layer-1 ====================================="
+bash scripts/long_term_forecast/Weather_script/abl_NewTimesNet/layer_1.sh
+
+echo "=============================== head-1 ====================================="
+bash scripts/long_term_forecast/Weather_script/abl_NewTimesNet/head_1.sh
+
+echo "=============================== head-2 ====================================="
+bash scripts/long_term_forecast/Weather_script/abl_NewTimesNet/head_2.sh
+
+echo "=============================== lambda_05 ====================================="
+bash scripts/long_term_forecast/Weather_script/abl_NewTimesNet/lambda_05.sh
