15be4b7b77aa3907c4db8e431b1d0a0a5e1de808
Akira37 <863781751@qq.com>
2025-10-22 16:13:14 +0800

    Update requirements.txt

--- test/hytest.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import torch
-
-if __name__ == '__main__':
-    print(np.__version__)
-    print(pd.__version__)
-    print(torch.__version__)
-    print(torch.cuda.is_available())
